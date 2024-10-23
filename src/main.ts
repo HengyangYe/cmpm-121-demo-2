44b517fe90e327afda713a65a7a3f52e53f3ab2b
HengyangYe <heye@ucsc.edu>
2024-10-23 01:57:03 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ const ctx = canvas.getContext("2d")!;
 let isDrawing = false;
 const lines: Array<Array<{ x: number; y: number }>> = [];
 let currentLine: Array<{ x: number; y: number }> = [];
+const redoStack: Array<Array<{ x: number; y: number }>> = [];
 
 // Draws the currently saved line
 function redraw() {
@@ -48,6 +49,7 @@ canvas.addEventListener("mousedown", () => {
   isDrawing = true;
   currentLine = [];
   lines.push(currentLine);
+  redoStack.length = 0; // Empty the redo stack because of the new drawing contents
 });
 
 canvas.addEventListener("mousemove", (event) => {
@@ -76,3 +78,44 @@ clearButton.addEventListener("click", () => {
   const eventChanged = new Event("drawing-changed");
   canvas.dispatchEvent(eventChanged); // Redrawing the emptied canvas
 });
+
+// Undo button
+const undoButton = document.createElement("button");
+undoButton.textContent = "Undo";
+app.appendChild(undoButton);
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    const undoneLine = lines.pop(); // Undo Recent Lines
+    if (undoneLine) {
+      redoStack.push(undoneLine); // Save to Redo Stack
+    }
+    const eventChanged = new Event("drawing-changed");
+    canvas.dispatchEvent(eventChanged);
+  }
+});
+
+// Redo button
+const redoButton = document.createElement("button");
+redoButton.textContent = "Redo";
+app.appendChild(redoButton);
+
+redoButton.addEventListener("click", () => {
+  if (redoStack.length > 0) {
+    const redoneLine = redoStack.pop(); // Take out the most recently undone line
+    if (redoneLine) {
+      lines.push(redoneLine); // Re-add to display list
+    }
+    const eventChanged = new Event("drawing-changed");
+    canvas.dispatchEvent(eventChanged);
+  }
+});
+
+// Format to look nice
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("button-container");
+app.appendChild(buttonContainer);
+
+buttonContainer.appendChild(clearButton);
+buttonContainer.appendChild(undoButton);
+buttonContainer.appendChild(redoButton);
