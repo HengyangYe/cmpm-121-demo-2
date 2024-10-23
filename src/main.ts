4645cac701c2288d8470ec8ca430478273c6e1d4
HengyangYe <heye@ucsc.edu>
2024-10-23 01:37:56 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,40 @@ app.appendChild(title);
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
-canvas.id = "appCanvas"; // To make it easier to apply styles in CSS
+canvas.id = "appCanvas";
 app.appendChild(canvas);
 
 const ctx = canvas.getContext("2d")!;
 let isDrawing = false;
+const lines: Array<Array<{ x: number; y: number }>> = [];
+let currentLine: Array<{ x: number; y: number }> = [];
+
+// Draws the currently saved line
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const line of lines) {
+    ctx.beginPath();
+    for (let i = 0; i < line.length; i++) {
+      const point = line[i];
+      if (i === 0) {
+        ctx.moveTo(point.x, point.y);
+      } else {
+        ctx.lineTo(point.x, point.y);
+      }
+    }
+    ctx.stroke();
+  }
+}
+
+// event for drawing-changed
+canvas.addEventListener("drawing-changed", () => {
+  redraw();
+});
 
 canvas.addEventListener("mousedown", () => {
   isDrawing = true;
-  ctx.beginPath();
+  currentLine = [];
+  lines.push(currentLine);
 });
 
 canvas.addEventListener("mousemove", (event) => {
@@ -30,13 +55,15 @@ canvas.addEventListener("mousemove", (event) => {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-  ctx.lineTo(x, y);
-  ctx.stroke();
+  currentLine.push({ x, y });
+  
+  // event for drawing-changed
+  const eventChanged = new Event("drawing-changed");
+  canvas.dispatchEvent(eventChanged);
 });
 
 canvas.addEventListener("mouseup", () => {
   isDrawing = false;
-  ctx.closePath();
 });
 
 // Create and add a clear button
@@ -45,5 +72,7 @@ clearButton.textContent = "Clear";
 app.appendChild(clearButton);
 
 clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.length = 0; // clear
+  const eventChanged = new Event("drawing-changed");
+  canvas.dispatchEvent(eventChanged); // Redrawing the emptied canvas
 });
