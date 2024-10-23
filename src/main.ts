3460e5091c9d4f66dcb7a016ad08208487216d74
HengyangYe <heye@ucsc.edu>
2024-10-23 01:02:58 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "We Are Young";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+// Title
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+// Create and add a canvas element
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "appCanvas"; // To make it easier to apply styles in CSS
+app.appendChild(canvas);
